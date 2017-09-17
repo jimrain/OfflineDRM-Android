e76167a062d5e71a0823d046fc0f8a18ea97be98
jimrain-bc <jrainville@brightcove.com>
2017-09-17 07:45:25 -0700

    Adding download functionality.

--- app/src/main/java/net/rainville/offlinedrm/MainActivity.java
+++ app/src/main/java/net/rainville/offlinedrm/MainActivity.java
@@ -1,9 +1,14 @@
 package net.rainville.offlinedrm;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Formatter;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.brightcove.player.edge.Catalog;
 import com.brightcove.player.edge.PlaylistListener;
@@ -15,10 +20,26 @@ import com.brightcove.player.view.BrightcoveExoPlayerVideoView;
 import com.brightcove.player.media.DeliveryType;
 import com.brightcove.player.model.Video;
 
+import com.brightcove.player.event.Event;
+import com.brightcove.player.media.DeliveryType;
+import com.brightcove.player.model.Video;
+import com.brightcove.player.network.DownloadManager;
+import com.brightcove.player.network.DownloadStatus;
+import com.brightcove.player.offline.DashDownloadable;
+import com.brightcove.player.offline.MediaDownloadable;
+import com.brightcove.player.offline.RequestConfig;
+import com.brightcove.player.store.DownloadRequest;
+import com.brightcove.player.view.BrightcovePlayer;
+
+import java.io.Serializable;
+import java.util.Map;
+
 public class MainActivity extends BrightcovePlayer {
 
+    private static final String TAG = "OfflineDRM";
     private Button mDownloadButton;
     private Button mPlayLocalButton;
+    private Video mTargetVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +54,13 @@ public class MainActivity extends BrightcovePlayer {
         mDownloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Do something useful.
+                RequestConfig requestConfig = new RequestConfig();
+                requestConfig.setNotificationVisibility(RequestConfig.VISIBILITY_VISIBLE);
+                requestConfig.setMobileDownloadAllowed(true);
+
+                // Create the DashDownloadable with the video object
+                DashDownloadable dashDownloadable = new DashDownloadable(getApplicationContext(), mTargetVideo, downloadEventListener, requestConfig);
+                dashDownloadable.requestDownload();
             }
         });
 
@@ -60,6 +87,7 @@ public class MainActivity extends BrightcovePlayer {
             public void onVideo(Video video) {
                 brightcoveVideoView.add(video);
                 brightcoveVideoView.start();
+                mTargetVideo = video;
             }
 
             @Override
@@ -91,6 +119,102 @@ public class MainActivity extends BrightcovePlayer {
             }
         });
 
+    }
+
+    /**
+     * Implements a {@link com.brightcove.player.offline.MediaDownloadable.DownloadEventListener} that
+     * will show a toast message about the download status and refresh the video list display.
+     */
+    private final MediaDownloadable.DownloadEventListener downloadEventListener = new MediaDownloadable.DownloadEventListener() {
+
+        @Override
+        public void onDownloadRequested(@NonNull final Video video) {
+            Log.i(TAG, String.format(
+                    "Starting to process '%s' video download request", video.getName()));
+            // videoListAdapter.notifyVideoChanged(video);
+        }
+
+        @Override
+        public void onDownloadStarted(@NonNull Video video, long estimatedSize, @NonNull Map<String, Serializable> mediaProperties) {
+            // videoListAdapter.notifyVideoChanged(video);
+            String message = showToast(
+                    "Started to download '%s' video. Estimated = %s, width = %s, height = %s, mimeType = %s",
+                    video.getName(),
+                    Formatter.formatFileSize(MainActivity.this, estimatedSize),
+                    mediaProperties.get(Event.RENDITION_WIDTH),
+                    mediaProperties.get(Event.RENDITION_HEIGHT),
+                    mediaProperties.get(Event.RENDITION_MIME_TYPE)
+            );
+            Log.i(TAG, message);
+        }
+
+        @Override
+        public void onDownloadProgress(@NonNull final Video video, @NonNull final DownloadStatus status) {
+            Log.i(TAG, String.format(
+                    "Downloaded %s out of %s of '%s' video. Progress %3.2f",
+                    Formatter.formatFileSize(MainActivity.this, status.getBytesDownloaded()),
+                    Formatter.formatFileSize(MainActivity.this, status.getMaxSize()),
+                    video.getName(), status.getProgress()));
+            // videoListAdapter.notifyVideoChanged(video, status);
+        }
+
+        @Override
+        public void onDownloadPaused(@NonNull final Video video, @NonNull final DownloadStatus status) {
+            Log.i(TAG, String.format(
+                    "Paused download of '%s' video: Reason #%d", video.getName(), status.getReason()));
+            // videoListAdapter.notifyVideoChanged(video, status);
+        }
+
+        @Override
+        public void onDownloadCompleted(@NonNull final Video video, @NonNull final DownloadStatus status) {
+            // videoListAdapter.notifyVideoChanged(video, status);
+            String message = showToast(
+                    "Successfully saved '%s' video", video.getName());
+            Log.i(TAG, message);
+        }
+
+        @Override
+        public void onDownloadCanceled(@NonNull final Video video) {
+            //No need to update UI here because it will be handled by the deleteVideo method.
+            String message = showToast(
+                    "Cancelled download of '%s' video removed", video.getName());
+            Log.i(TAG, message);
+            // onDownloadRemoved(video);
+        }
+
+        @Override
+        public void onDownloadDeleted(@NonNull final Video video) {
+            //No need to update UI here because it will be handled by the deleteVideo method.
+            String message = showToast(
+                    "Offline copy of '%s' video removed", video.getName());
+            Log.i(TAG, message);
+            // onDownloadRemoved(video);
+        }
+
+        @Override
+        public void onDownloadFailed(@NonNull final Video video, @NonNull final DownloadStatus status) {
+            // videoListAdapter.notifyVideoChanged(video, status);
+            String message = showToast(
+                    "Failed to download '%s' video: Error #%d", video.getName(), status.getReason());
+            Log.e(TAG, message);
+        }
+    };
+
+    /**
+     * Shows a formatted toast message.
+     *
+     * @param message    the message to be shown. The message may include string format tokens.
+     * @param parameters the parameters to be used for formatting the message.
+     * @return the formatted message that was shown.
+     * @see String#format(String, Object...)
+     */
+    private String showToast(@NonNull String message, @Nullable Object... parameters) {
+        if (parameters != null) {
+            message = String.format(message, parameters);
+        }
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+
+        return message;
     }
 
 }
